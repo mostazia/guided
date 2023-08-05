2c8e8d216c6f4a707fe19893605a33c5968be15c
mostazia <lililopez9879@gmail.com>
2023-08-04 21:36:05 -0700

    testing images

--- JS/products.js
+++ JS/products.js
@@ -1,6 +1,71 @@
+var _10000009 = {
+  code: 10000009,
+  name: "Unit Eight (Ecology) One Pager",
+  subject: "AP Biology",
+  new: true,
+  im1: "Images/10000009_1",
+};
+
+var _10000008 = {
+  code: 10000008,
+  name: "Unit Seven (Natural Selection) One Pager",
+  subject: "AP Biology",
+  new: true,
+  im1: "Images/10000008_1",
+};
+
+var _10000007 = {
+  code: 10000007,
+  name: "Unit Six (Gene Expression and Regulation) One Pager",
+  subject: "AP Biology",
+  new: true,
+  im1: "Images/10000007_1",
+};
+
+var _10000006 = {
+  code: 10000006,
+  name: "Unit Five (Heredity) One Pager",
+  subject: "AP Biology",
+  new: true,
+  im1: "Images/10000006_1",
+};
+
+var _10000005 = {
+  code: 10000005,
+  name: "Unit Four (Cell Communication and Cell Cycle) One Pager",
+  subject: "AP Biology",
+  new: true,
+  im1: "Images/10000005_1",
+};
+
+var _10000004 = {
+  code: 10000004,
+  name: "Unit Three (Cellular Energetics) One Pager",
+  subject: "AP Biology",
+  new: true,
+  im1: "Images/10000004_1",
+};
+
+var _10000003 = {
+  code: 10000003,
+  name: "Unit Two (Cell Structure and Function) One Pager",
+  subject: "AP Biology",
+  new: true,
+  im1: "Images/10000003_1",
+};
+
+var _10000002 = {
+  code: 10000002,
+  name: "Unit One (Biochemistry) One Pager",
+  subject: "AP Biology",
+  new: true,
+  im1: "Images/10000002_1",
+};
+
 var _10000001 = {
   code: 10000001,
   name: "One Pagers Bundle",
   subject: "AP Biology",
   new: true,
+  im1: "Images/10000001_1",
 };
